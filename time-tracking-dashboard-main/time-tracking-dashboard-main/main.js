1dc128c023e9c87209eb2dd7f8a4b59357727280
Johnson <kameron.johnson@hp.com>
2023-12-08 15:54:17 -0600

    working on js

--- time-tracking-dashboard-main/time-tracking-dashboard-main/main.js
+++ time-tracking-dashboard-main/time-tracking-dashboard-main/main.js
@@ -1,22 +1,42 @@
-
 // This function sets the background image icons to each div container
-document.addEventListener("DOMContentLoaded", function() {
-    // Array of image paths
-    const imagePaths = [
-        "./images/icon-work.svg",
-        "./images/icon-play.svg",
-        "./images/icon-study.svg",
-        "./images/icon-exercise.svg",
-        "./images/icon-social.svg",
-        "./images/icon-self-care.svg"
-    ];
-  
-    // Select all elements with the class 'image-div'
-    const imageDivs = document.querySelectorAll(".tracking-divs");
-  
-    // Use forEach to loop through each div and set the background image
-    imageDivs.forEach((div, index) => {
-      div.style.backgroundImage = `url('${imagePaths[index]}')`;
-    });
+document.addEventListener("DOMContentLoaded", function () {
+  // Array of image paths
+  const imagePaths = [
+    "./images/icon-work.svg",
+    "./images/icon-play.svg",
+    "./images/icon-study.svg",
+    "./images/icon-exercise.svg",
+    "./images/icon-social.svg",
+    "./images/icon-self-care.svg",
+  ];
+
+  // Select all elements with the class 'image-div'
+  const imageDivs = document.querySelectorAll(".tracking-divs");
+
+  // Use forEach to loop through each div and set the background image
+  imageDivs.forEach((div, index) => {
+    div.style.backgroundImage = `url('${imagePaths[index]}')`;
   });
-  
+});
+
+
+// Fetching JSON
+const jsonPath = '/data.json';
+// const jsonPath = './data.json';
+// const jsonPath = 'http://127.0.0.1:8080/data.json';
+
+fetch(jsonPath)
+  .then(res => {
+    if(!res.ok) {
+      throw new Error(`HTTP error! Status: ${res.status}`);
+    }
+      return res.json();
+  })
+  .then(data => {
+    console.log(data);
+  })
+  .catch(error => {
+    console.log("Fetch error:", error);
+  })
+
+  console.log(jsonPath);
