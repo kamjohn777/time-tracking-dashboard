ea1aa03869e61857f4ebd1545eb93ead27cd4bf0
Johnson <kameron.johnson@hp.com>
2023-12-08 16:16:46 -0600

    finished updating display

--- time-tracking-dashboard-main/time-tracking-dashboard-main/main.js
+++ time-tracking-dashboard-main/time-tracking-dashboard-main/main.js
@@ -19,24 +19,269 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 });
 
+// function for displaying different data
+const displayFunction = () => {
+  // Element IDs daily,weekly,monthly view
+  const dailyView = document.getElementById("daily-elm");
+  const weeklyView = document.getElementById("weekly-elm");
+  const monthlyView = document.getElementById("monthly-elm");
 
-// Fetching JSON
-const jsonPath = '/data.json';
-// const jsonPath = './data.json';
-// const jsonPath = 'http://127.0.0.1:8080/data.json';
+  // JSON Data
+  const jsonData = [
+    {
+      title: "Work",
+      timeframes: {
+        daily: {
+          current: 5,
+          previous: 7,
+        },
+        weekly: {
+          current: 32,
+          previous: 36,
+        },
+        monthly: {
+          current: 103,
+          previous: 128,
+        },
+      },
+    },
+    {
+      title: "Play",
+      timeframes: {
+        daily: {
+          current: 1,
+          previous: 2,
+        },
+        weekly: {
+          current: 10,
+          previous: 8,
+        },
+        monthly: {
+          current: 23,
+          previous: 29,
+        },
+      },
+    },
+    {
+      title: "Study",
+      timeframes: {
+        daily: {
+          current: 0,
+          previous: 1,
+        },
+        weekly: {
+          current: 4,
+          previous: 7,
+        },
+        monthly: {
+          current: 13,
+          previous: 19,
+        },
+      },
+    },
+    {
+      title: "Exercise",
+      timeframes: {
+        daily: {
+          current: 1,
+          previous: 1,
+        },
+        weekly: {
+          current: 4,
+          previous: 5,
+        },
+        monthly: {
+          current: 11,
+          previous: 18,
+        },
+      },
+    },
+    {
+      title: "Social",
+      timeframes: {
+        daily: {
+          current: 1,
+          previous: 3,
+        },
+        weekly: {
+          current: 5,
+          previous: 10,
+        },
+        monthly: {
+          current: 21,
+          previous: 23,
+        },
+      },
+    },
+    {
+      title: "Self Care",
+      timeframes: {
+        daily: {
+          current: 0,
+          previous: 1,
+        },
+        weekly: {
+          current: 2,
+          previous: 2,
+        },
+        monthly: {
+          current: 7,
+          previous: 11,
+        },
+      },
+    },
+  ];
+};
 
-fetch(jsonPath)
-  .then(res => {
-    if(!res.ok) {
-      throw new Error(`HTTP error! Status: ${res.status}`);
-    }
-      return res.json();
-  })
-  .then(data => {
-    console.log(data);
-  })
-  .catch(error => {
-    console.log("Fetch error:", error);
-  })
+ // JSON Data
+ const jsonData = [
+  {
+    title: "Work",
+    timeframes: {
+      daily: {
+        current: 5,
+        previous: 7,
+      },
+      weekly: {
+        current: 32,
+        previous: 36,
+      },
+      monthly: {
+        current: 103,
+        previous: 128,
+      },
+    },
+  },
+  {
+    title: "Play",
+    timeframes: {
+      daily: {
+        current: 1,
+        previous: 2,
+      },
+      weekly: {
+        current: 10,
+        previous: 8,
+      },
+      monthly: {
+        current: 23,
+        previous: 29,
+      },
+    },
+  },
+  {
+    title: "Study",
+    timeframes: {
+      daily: {
+        current: 0,
+        previous: 1,
+      },
+      weekly: {
+        current: 4,
+        previous: 7,
+      },
+      monthly: {
+        current: 13,
+        previous: 19,
+      },
+    },
+  },
+  {
+    title: "Exercise",
+    timeframes: {
+      daily: {
+        current: 1,
+        previous: 1,
+      },
+      weekly: {
+        current: 4,
+        previous: 5,
+      },
+      monthly: {
+        current: 11,
+        previous: 18,
+      },
+    },
+  },
+  {
+    title: "Social",
+    timeframes: {
+      daily: {
+        current: 1,
+        previous: 3,
+      },
+      weekly: {
+        current: 5,
+        previous: 10,
+      },
+      monthly: {
+        current: 21,
+        previous: 23,
+      },
+    },
+  },
+  {
+    title: "Self Care",
+    timeframes: {
+      daily: {
+        current: 0,
+        previous: 1,
+      },
+      weekly: {
+        current: 2,
+        previous: 2,
+      },
+      monthly: {
+        current: 7,
+        previous: 11,
+      },
+    },
+  },
+];
 
-  console.log(jsonPath);
+
+  // Function to update content based on the selected timeframe
+  // const updateContent = (timeframe) => {
+  //   jsonData.forEach((data) => {
+  //     console.log(`${data.title} - ${data.timeframes[timeframe].current} hours`);
+      
+  //     if()
+  //   });
+  // };
+
+  const dailyView = document.getElementById("daily-elm");
+  const weeklyView = document.getElementById("weekly-elm");
+  const monthlyView = document.getElementById("monthly-elm");
+
+
+
+  // Function to update content based on the selected timeframe
+const updateContent = (timeframe) => {
+  jsonData.forEach((data, index) => {
+    const elementId = `track-${index + 1}`;
+
+    // Get the container element for the current activity
+    const activityContainer = document.getElementById(elementId);
+
+    // Update the content based on the selected timeframe
+    const content = `
+      <div class="track-text-wrap">
+        <p>${data.title}</p>
+        <h2>${data.timeframes[timeframe].current} hours</h2>
+        <p>Last Week - ${data.timeframes[timeframe].previous} hours</p>
+      </div>
+    `;
+
+    // Replace the existing content with the updated content
+    activityContainer.innerHTML = content;
+  });
+};
+
+// Event listeners for the timeframe options
+dailyView.addEventListener("click", () => updateContent("daily"));
+weeklyView.addEventListener("click", () => updateContent("weekly"));
+monthlyView.addEventListener("click", () => updateContent("monthly"));
+
+// Initial setup, e.g., show daily content by default
+updateContent("daily");
+
